9e03733555ce56f4bb65353bc4c3b2d3ed566718
damian <godevdamian@gmail.com>
2021-09-04 16:17:48 +0200

    Added sticky navigation

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,28 +1,53 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useRef, useEffect } from 'react';
 import styles from './navigation.module.scss';
 import Link from 'next/link';
 import { useTranslation } from '../../hooks/useTranslation';
 
 const index = (): JSX.Element => {
   const [isActive, setIsActive] = useState(false);
+  const hamburgerWrapperRef = useRef<HTMLDivElement>(null);
+  const navigationWrapperRef = useRef<HTMLDivElement>(null);
   const handleNavItemClick = useCallback((): void => {
     setIsActive(!isActive);
   }, [isActive]);
 
   const { dictionary } = useTranslation();
 
+  const handleTopBarSticky = () => {
+    const hamburgerRef = hamburgerWrapperRef.current;
+    const navigationDesktopRef = navigationWrapperRef.current;
+    const pageYOffset= window.pageYOffset;
+
+    if (pageYOffset * 2 > hamburgerRef.offsetHeight || pageYOffset * 2 > navigationDesktopRef.offsetHeight) {
+      hamburgerRef.classList.add(styles.hamburger__wrapper__sticky);
+      navigationDesktopRef.classList.add(styles.navigation__wrapper__sticky);
+    } else {
+      hamburgerRef.classList.remove(styles.hamburger__wrapper__sticky);
+      navigationDesktopRef.classList.remove(styles.navigation__wrapper__sticky);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleTopBarSticky);
+
+    return () => window.removeEventListener('scroll', handleTopBarSticky);
+  }, []);
+
   return (
     <>
-      <button className={styles.hamburger} onClick={handleNavItemClick}>
-        <span className={styles.hamburger__box}>
-          <span
-            className={`${styles.hamburger__inner} ${
-              isActive && styles.hamburger__active
-            }`}
-          />
-        </span>
-      </button>
+      <div className={`${styles.hamburger__wrapper} ${isActive && styles.hamburger__wrapper__transparent}`} ref={hamburgerWrapperRef}>
+        <button className={styles.hamburger} onClick={handleNavItemClick}>
+          <span className={styles.hamburger__box}>
+            <span
+              className={`${styles.hamburger__inner} ${
+                isActive && styles.hamburger__active
+              }`}
+            />
+          </span>
+        </button>
+      </div>
       <div
+        ref={navigationWrapperRef}
         className={`${styles.navigation__wrapper} ${
           isActive && styles.navigation__wrapper__active
         }`}
@@ -35,7 +60,11 @@ const index = (): JSX.Element => {
               </a>
             </Link>
           </div>
-          <ul className={`${styles.navigation__list} ${isActive && styles.active__mobile__menu}`}>
+          <ul
+            className={`${styles.navigation__list} ${
+              isActive && styles.active__mobile__menu
+            }`}
+          >
             <li onClick={handleNavItemClick}>
               <Link href="#home">
                 <a>{dictionary.components.menu.labels.home}</a>
