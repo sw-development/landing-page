e9225a7fe0001883a1fdcb14dcb3a9753426cf74
damian <godevdamian@gmail.com>
2021-09-20 17:46:15 +0200

    Footer changed

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <footer className={styles.wrapper}>
       <span>&copy; 2021 SW Development</span>
+      <a className={styles.footer__email} href="mailto:swdevelopmentinfo@gmail.com">swdevelopmentinfo@gmail.com</a>
     </footer>
   );
 };
