0b857c75f7d157c43bc058c0280806b279f730b7
damian <godevdamian@gmail.com>
2021-10-26 19:04:06 +0200

    Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '@/../../styles/normalize.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>SW Development</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
