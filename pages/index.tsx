47ac6830db1bf61590790d71833af69f6fcd83fd
damian <godevdamian@gmail.com>
2022-01-20 21:10:36 +0100

    Fetching blogposts from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,44 @@ import Business from '@/../../components/Business';
 import Footer from '../components/Footer';
 import Subscribe from '../components/Subscribe';
 import Blog from '@/../../components/Blog';
+import { GraphQLClient, gql } from 'graphql-request';
 
-export default function Home() {
+
+
+export async function getStaticProps() {
+  const client = new GraphQLClient(
+    process.env.NEXT_PUBLIC_GRAPH_CMS_API,
+    {
+      headers: {
+        Authorization:
+        `Bearer ${process.env.NEXT_PUBLIC_GRAPH_CMS_TOKEN}`
+      },
+    }
+  );
+
+  const query = gql`
+    query {
+      blogPosts(first: 6) {
+        slug
+        timestamp
+        title
+        description
+      }
+    }
+  `;
+
+  const results = await client.request(query);
+
+  return {
+    props: {
+      results,
+    },
+  };
+
+}
+
+export default function Home({ results }) {
+  console.log(results);
   return (
     <>
       <Navigation />
