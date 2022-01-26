c3af1cc481b3bcab9378518c20bdbdfdfd43251f
damian <godevdamian@gmail.com>
2022-01-26 17:25:12 +0100

    fetching info about specific post

--- pages/blog/post/[slug].tsx
+++ pages/blog/post/[slug].tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 import Navigation from '@/../../components/Navigation/index';
 import Footer from '@/../../components/Footer';
 import Subscribe from '@/../../components/Subscribe';
 import PostContent from '@/../../components/Post/Post';
 import { useBlogPosts } from '../../../context/blogPosts/blogPosts';
+import { GraphQLClient, gql } from 'graphql-request';
+import { Post as PostModel } from '../../../context/blogPosts/intefaces';
+import { NextPageContext } from 'next';
 
-const Post = () => {
-  const { slug } = useRouter().query;
-  const { state } = useBlogPosts();
+interface ComponentProps {
+  result: PostModel;
+}
 
-  console.log(state);
+const Post = (props: ComponentProps) => {
+  console.log(props);
   return (
     <>
       <Navigation />
@@ -19,6 +22,40 @@ const Post = () => {
       <Footer />
     </>
   );
+};
+
+Post.getInitialProps = async (context: NextPageContext) => {
+  const client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPH_CMS_API, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPH_CMS_TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    query($slug: String!) {
+      bLOGPOST(where: {slug: $slug}) {
+        id
+        title
+        slug
+        description
+        previewImg {
+          url
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    slug: context.query.slug
+  }
+
+  const { bLOGPOST } = await client.request(query, variables);
+
+  return {
+    props: {
+      result: bLOGPOST
+    },
+  };
 };
 
 export default Post;
